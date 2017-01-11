5fde2520aade99e2d8b24b2b8c17ba387bcb4316
Rob Hough <robert.hough@nextgearcapital.com>
2017-01-11 14:25:28 -0500

    propgating vgws added back

--- variables.tf
+++ variables.tf
@@ -33,3 +33,13 @@ variable "map_public_ip_on_launch" {
   description = "should be false if you do not want to auto-assign public IP on launch"
   default     = true
 }
+
+variable "private_propagating_vgws" {
+  description = "A list of VGWs the private route table should propagate."
+  default     = []
+}
+
+variable "public_propagating_vgws" {
+  description = "A list of VGWs the public route table should propagate."
+  default     = []
+}
